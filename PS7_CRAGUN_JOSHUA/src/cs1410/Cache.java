7881ffe036a2d38a45f313ff35b7e67718c6ad3f
jmcragun <jmcragun@gmail.com>
2017-10-25 15:00:21 -0600

    Mid update

--- PS7_CRAGUN_JOSHUA/src/cs1410/Cache.java
+++ PS7_CRAGUN_JOSHUA/src/cs1410/Cache.java
@@ -1,5 +1,7 @@
 package cs1410;
 
+import java.util.Scanner;
+
 /**
  * Represents a variety of information about a geocache. A geocache has a title, an owner, a difficulty rating, a
  * terrain rating, a GC code, a latitude, and a longitude.
@@ -7,13 +9,13 @@ package cs1410;
 public class Cache
 {
     // TODO: Put representation here
-    String gcCode;
-    String title;
-    String owner;
-    double difficulty;
-    double terrain;
-    double latitude;
-    double longitude;
+    private String gcCode;
+    private String title;
+    private String owner;
+    private double difficulty;
+    private double terrain;
+    private double latitude;
+    private double longitude;
 
     /**
      * Creates a Cache from a string that consists of these seven cache attributes: the GC code, the title, the owner,
@@ -22,16 +24,61 @@ public class Cache
      * 
      * If any of the following problems are present, throws an IllegalArgumentException:
      * <ul>
-     * <li>Fewer than seven attributes</li>
-     * <li>More than seven attributes</li>
+     * <li>Fewer than seven attributes</li> (done)
+     * <li>More than seven attributes</li> (done)
      * <li>A GC code that is anything other than "GC" followed by one or more upper-case letters and/or digits</li>
      * <li>A difficulty or terrain rating that parses to anything other than the doubles 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5,
      * or 5.</li>
-     * <li>A title, owner, latitude, or longitude that consists only of white space</li>
+     * <li>A title, owner, latitude, or longitude that consists only of white space</li> (done)
      */
     public Cache (String attributes)
     {
         // TODO: Implement
+        try
+        {
+            Scanner scnr = new Scanner(attributes);
+            gcCode = scnr.next();
+            title = scnr.next();
+            owner = scnr.next();
+            difficulty = Double.parseDouble(scnr.next());
+            terrain = Double.parseDouble(scnr.next());
+            latitude = Double.parseDouble(scnr.next());
+            longitude = Double.parseDouble(scnr.next());
+            if (scnr.hasNext())
+            {
+                scnr.close();
+                throw new IllegalArgumentException();
+            }
+            scnr.close();
+        }
+        catch (Exception e)
+        {
+            throw new IllegalArgumentException();
+        }
+        if (!verifyGcCode(gcCode)) {
+            throw new IllegalArgumentException();
+        }
+
+    }
+
+    /**
+     * Private helper method that makes sure the code is "GC" followed by one or more upper-case letters and/or digits.
+     * 
+     * @param code
+     * @return
+     */
+    private static boolean verifyGcCode (String code)
+    {
+        String front = code.substring(0, 2);
+        if (!front.equals("GC"))
+        {
+            return false;
+        }
+        if (!code.matches("[A-Z]|[0-9]"))
+        {
+            return false;
+        }
+        return true;
     }
 
     /**
